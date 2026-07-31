02a3b74628d774c024e1be1134e3e1efbd67ec19
Roberto Santizo <soportetecnico.tejar@legumex.net>
2026-07-31 16:11:51 -0600

    feat: implement JWTSubject interface in User model for JWT authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,10 +10,11 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
 #[Fillable(['name', 'email', 'password', 'role'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
@@ -29,6 +30,29 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
             'role' => UserRole::class,
+        ];
+    }
+
+    /**
+     * Get the identifier that will be stored in the "sub" claim of the JWT.
+     */
+    public function getJWTIdentifier(): mixed
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Get the custom claims added to the JWT payload.
+     *
+     * @return array{id: int, name: string, email: string, role: string}
+     */
+    public function getJWTCustomClaims(): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'role' => $this->role->value,
         ];
     }
 }
