026012b19d8c54976177459b6d72217d6ed88810
Roberto Santizo <soportetecnico.tejar@legumex.net>
2026-08-04 15:25:40 -0600

    feat: add CarrierProvider for binding CarrierServiceInterface to CarrierService

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,10 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\Auth\AuthProvider;
+use App\Providers\Carrier\CarrierProvider;
 
 return [
     AppServiceProvider::class,
     AuthProvider::class,
+    CarrierProvider::class,
 ];
