99c14b4fa3c97c0a1580706bb6511f95bff3f6a5
Roberto Santizo <soportetecnico.tejar@legumex.net>
2026-08-04 15:10:23 -0600

    feat: add CarrierPilot model and migration for carrier_pilots table

---
+++ app/Models/CarrierPilot.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+
+#[Fillable(['carrier_id', 'user_id'])]
+class CarrierPilot extends Model
+{
+    //
+}
