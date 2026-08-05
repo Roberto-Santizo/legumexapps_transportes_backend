3b197faeaeee1cd8ca16a5a62d0163aad439a8b0
Roberto Santizo <soportetecnico.tejar@legumex.net>
2026-08-05 14:48:04 -0600

    feat: add vehicles relationship to Carrier model

--- app/Models/Carrier.php
+++ app/Models/Carrier.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[Fillable(['user_id', 'name', 'image', 'code', 'active'])]
 class Carrier extends Model
@@ -33,6 +34,16 @@ class Carrier extends Model
     public function pilots(): BelongsToMany
     {
         return $this->belongsToMany(User::class, 'carrier_pilots')->withTimestamps();
+    }
+
+    /**
+     * The vehicles owned by this company.
+     *
+     * @return HasMany<Vehicle, $this>
+     */
+    public function vehicles(): HasMany
+    {
+        return $this->hasMany(Vehicle::class);
     }
 
     /**
